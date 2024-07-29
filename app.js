d39d15a10f726065d99a1ee9d7c8f2e1fdc13679
inversionesMadagagaskar <inversionesmadagaskar@gmail.com>
2024-07-29 10:42:56 -0400

    update

---
+++ app.js
@@ -0,0 +1,26 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const  QRPortal = require('@bot-whatsapp/portal')
+
+const WebWhatsappProvider = require('@bot-whatsapp/provider/web-whatsapp')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const flowPrincipal = addKeyword(['hola', 'alo'])
+    .addAnswer(['Hola, bienvenido a mi tienda', '¿Como puedo ayudarte?'])
+    .addAnswer(['Tengo:', 'Zapatos', 'Bolsos', 'etc ...'])
+
+/**
+ * Esta es la función principal, ¡Importante, es la que realmente inicia el chatbot!
+ */
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(WebWhatsappProvider)
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+}
+
+QRPortal()
+main()
